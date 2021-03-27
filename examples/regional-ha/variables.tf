bb3951c44e046d2df2555e49548b8dc4e407fa93
Victor Yang <vyang@ignw.io>
2021-03-26 19:40:39 -0700

    add regiona-ha

---
+++ examples/regional-ha/variables.tf
@@ -0,0 +1,8 @@
+variable "project" {}
+variable "region" {}
+variable "namespace" {}
+variable "tfe_license_name" {}
+variable "tfe_license_path" {}
+variable "fqdn" {}
+variable "ssl_certificate_name" {}
+variable "dns_zone_name" {}
